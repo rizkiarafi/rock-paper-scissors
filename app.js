9ed7672fd9a4a608db4f0355d42132c47f0a5948
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-02-14 19:10:22 +0700

    Add 5 rounds comprised match

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 let humanScore = 0;
-let computerScore = 0;
+let botScore = 0;
+let roundCount = 0;
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+playGame();
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+  const humanSelection = getHumanChoice();
+  const botSelection = getBotChoice();
 
-function getComputerChoice() {
+  console.log(`||Round ${roundCount + 1}||`);
+
+  playRound(humanSelection, botSelection);
+
+  if (roundCount >= 5) {
+    if (humanScore === botScore) console.log("The match has ended evenly...");
+    else if (humanScore > botScore) console.log("You've won the match!");
+    else console.log("You've lost the match!");
+  } else {
+    playGame();
+  }
+
+  function playRound(humanChoice, botChoice) {
+    if (humanChoice.toLowerCase() === "rock") {
+      getResult(botChoice, "draw", "lose", "win");
+    } else if (humanChoice.toLowerCase() === "paper") {
+      getResult(botChoice, "win", "draw", "lose");
+    } else if (humanChoice.toLowerCase() === "scissors") {
+      getResult(botChoice, "lose", "win", "draw");
+    }
+    roundCount++;
+    console.log(`Your score: ${humanScore} || Bot score: ${botScore}`);
+  }
+
+  function getResult(botChoice, result1, result2, result3) {
+    console.log(`Bot choice is: ${botChoice}`);
+    switch (botChoice.toLowerCase()) {
+      case "rock":
+        console.log(checkResult(result1));
+        break;
+      case "paper":
+        console.log(checkResult(result2));
+        break;
+      case "scissors":
+        console.log(checkResult(result3));
+        break;
+    }
+  }
+
+  function checkResult(result) {
+    if (result === "win") {
+      humanScore++;
+      return "You've won this round!";
+    } else if (result === "lose") {
+      botScore++;
+      return "You've lost this round!";
+    } else {
+      return "It's a draw!";
+    }
+  }
+}
+
+function getBotChoice() {
   const randomNum = Math.random();
 
   if (randomNum < 1 / 3) {
@@ -20,41 +74,4 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   return prompt("Choose your hand sign!");
-}
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice.toLowerCase() === "rock") {
-    getResult(computerChoice, "draw", "lose", "win");
-  } else if (humanChoice.toLowerCase() === "paper") {
-    getResult(computerChoice, "win", "draw", "lose");
-  } else if (humanChoice.toLowerCase() === "scissors") {
-    getResult(computerChoice, "lose", "win", "draw");
-  }
-}
-
-function getResult(computerChoice, result1, result2, result3) {
-  console.log(`Computer choice is: ${computerChoice}`);
-  switch (computerChoice.toLowerCase()) {
-    case "rock":
-      console.log(checkResult(result1));
-      break;
-    case "paper":
-      console.log(checkResult(result2));
-      break;
-    case "scissors":
-      console.log(checkResult(result3));
-      break;
-  }
-}
-
-function checkResult(result) {
-  if (result === "win") {
-    humanScore++;
-    return "You've won this round!";
-  } else if (result === "lose") {
-    computerScore++;
-    return "You've lost this round!";
-  } else {
-    return "It's a draw!";
-  }
 }
