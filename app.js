6fb7dba51ca2d4579ddaf0ab9e7ed5d3606aec49
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-02 14:26:51 +0700

    Set up rules through objects for the game

---
+++ app.js
@@ -0,0 +1,5 @@
+const rules = {
+  rock: { rock: "draw", paper: "lose", scissors: "win" },
+  paper: { rock: "win", paper: "draw", scissors: "lose" },
+  scissors: { rock: "lose", paper: "win", scissors: "draw" },
+};
