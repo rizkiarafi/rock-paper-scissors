69a84bd094a5d5b5c5543b149d4874adbe04ce19
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-02 14:56:26 +0700

    Make five playable rounds per game
    - Round is repeated five times
    - Each win of a single round increments the winner score

--- app.js
+++ app.js
@@ -4,6 +4,45 @@ const rules = {
   scissors: { rock: "lose", paper: "win", scissors: "draw" },
 };
 
+playGame();
+
+function playGame() {
+  let humanScore = 0;
+  let botScore = 0;
+  let roundCounter = 1;
+
+  while (roundCounter <= 5) {
+    playRound();
+    roundCounter++;
+  }
+
+  function playRound() {
+    const humanSelection = getHumanChoice().toLowerCase();
+    const botSelection = getBotChoice().toLowerCase();
+    const gameResult = rules[humanSelection][botSelection];
+
+    console.log(`||Round ${roundCounter}||`);
+    console.log(`Human: ${humanSelection} || Bot: ${botSelection}`);
+    showResult(gameResult);
+    console.log(`Human score: ${humanScore} || Bot score: ${botScore}`);
+  }
+
+  function showResult(gameResult) {
+    switch (gameResult) {
+      case "win":
+        humanScore++;
+        console.log("You've won the match!");
+        break;
+      case "lose":
+        botScore++;
+        console.log("You've lost against the bot!");
+        break;
+      default:
+        console.log("The match has ended evenly");
+    }
+  }
+}
+
 function getHumanChoice() {
   return prompt("What is your handsign?");
 }
